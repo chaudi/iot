47ccf43bf77fb0d95993ff29b9d0fd207082b857
chaudi <cederfelt@live.com>
2018-12-09 22:02:54 +0100

    checking protocol

--- src/devices/Bmp280/IBmp280.cs
+++ src/devices/Bmp280/IBmp280.cs
@@ -6,5 +6,16 @@ namespace Iot.Device
 {
     public interface IBmp280
     {
+        double ReadTemperature();
+
+        double ReadPreasure();
+
+        double ReadAltitude(double seaLevel);
+
+        byte Read8(byte register);
+
+        ushort Read16(byte register);
+
+        uint Read24(byte register);
     }
 }
