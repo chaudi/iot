a118c5493a9df67d3e43bbbb637fe282acf9a67c
chaudi <cederfelt@live.com>
2018-12-09 21:36:46 +0100

    bmp280 implementation

---
+++ src/devices/Bmp280/IBmp280.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Iot.Device
+{
+    public interface IBmp280
+    {
+        double ReadTemperature();
+
+        double ReadPreasure();
+
+        double ReadAltitude(double seaLevel);
+
+        byte Read8(byte register);
+
+        ushort Read16(byte register);
+
+        uint Read24(byte register);
+    }
+}
---
+++ src/devices/Bmp280/samples/Bmp280.sample.cs
@@ -0,0 +1,43 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Iot.Device;
+using System;
+using System.Device.I2c;
+using System.Device.I2c.Drivers;
+using System.Threading;
+
+namespace Iot.Devices.Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Bmp280!");
+
+            const int bmp280Address = 0x77;
+            const int busId = 1;
+            const double defaultSeaLevelPressure = 1013.25;
+
+            var i2cSettings = new I2cConnectionSettings(busId, bmp280Address);
+            var unixDevice = new UnixI2cDevice(i2cSettings);
+            var i2CBmp280 = new Bmp280(unixDevice);
+
+            using (i2CBmp280)
+            {
+                while (true)
+                {
+                    double tempValue = i2CBmp280.ReadTemperature();
+                    Console.WriteLine($"Temperature {tempValue}");
+                    double preValue = i2CBmp280.ReadPressure();
+                    Console.WriteLine($"Preasure {preValue}");
+                    double altValue = i2CBmp280.ReadAltitude(defaultSeaLevelPressure);
+                    Console.WriteLine($"Altitude: {altValue}");
+                    Thread.Sleep(1000);
+                }
+            }
+
+        }
+    }
+}
